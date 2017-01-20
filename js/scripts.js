dfb44c8a4bead5cebf70e756e0387f5c3fdabce2
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-01-20 09:33:28 -0800

    add for loop that runs through all numbers in array and replaces numbers divisible by 15 with pingpong.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 var countDown = function(number) {
   var countDownArray = [];
-  for(index = number-1; index >= 0; index--) {
-    countDownArray.push(number - index);
+
+  for(i = number-1; i >= 0; i--) {
+    countDownArray.push(number - i);
   }
+
+  var arrayLength = countDownArray.length;
+
+  for(j = 0; j < arrayLength; j++) {
+    if (countDownArray[j] % 15 === 0) {
+      countDownArray.splice(countDownArray[j-1], 1, "pingpong");
+    }
+  }
+
 
   countDownArray.splice(2, 1, "ping");
   countDownArray.splice(4, 1, "pong");
