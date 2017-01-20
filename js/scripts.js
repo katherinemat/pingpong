eeac3a049436b94cd4e10ecf7fc46a93aeee0710
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-01-20 08:57:14 -0800

    create function that returns number that user enters.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
+var countDown = function(number) {
+  return number;
+}
+
+
 $(document).ready(function() {
   $("form#inputNumber").submit(function(event) {
     event.preventDefault();
-    var result = parseInt($("input#number").val());
+    var userInput = parseInt($("input#number").val());
+    var result = countDown(userInput);
     $("#results").text(result);
   });
 });
